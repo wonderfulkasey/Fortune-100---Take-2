459b7ac19eeacb019c22699de0753c9887ffa172
Kasey Arnold <kaseyarnold15@gmail.com>
2020-03-06 18:58:09 +0000

    added environment

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/fortune/scraper'
+require_relative '../lib/fortune/company'
+require_relative '../lib/fortune/cli'
+require_relative '../lib/fortune/version'
